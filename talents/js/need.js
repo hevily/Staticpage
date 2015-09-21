14c1c8a52a01bbdc4d5a243dc3a9187ea8ab3159
Lei <617054896@qq.com>
2015-09-21 23:54:38 +0800

    talents needs

--- talents/js/need.js
+++ talents/js/need.js
@@ -4,26 +4,38 @@ $(document).ready(function(){
 		console.log("标签被点击");
 		$(".active-tag").removeClass("active-tag");
 		$(this).addClass("active-tag");
+		$(".tagSelect").hide();
+				$(".selected-tag").show();
+
+		resiezInput();
+		// resizeTalentsinfo();
 	})
+	$(".next-step input").click(function() {
+		$(".step-one").hide();
+		$(".step-two").show();
+		resiezInput();
+		// resizeTalentsinfo();
+	});
 	// 工作推送
 	$(document).delegate(".waitSend","click",function(){
 		$(this).addClass('hidden');
+		
 		$(this).next(".sended").removeClass('hidden');
 	})
 	function resiezInput(){
 		var inputArray = $(".item-details input");
 		for (var i = 0;i<=inputArray.length - 1; i++) {
 			var thisInputP = $(inputArray[i]).closest(".item-details");
-			thisInputP.width($(".edit-resume").width()-thisInputP.prev(".item-title").width()-10);
+			thisInputP.width($(".container").width()-thisInputP.prev(".item-title").width()-30);
 		};
-	}resiezInput();
+	}
 	function resizeTalentsinfo(){
 		var talentsinfoArray = $(".talents-info");
 		for (var i = 0;i<=talentsinfoArray.length - 1; i++) {
 			var thisInfo = $(talentsinfoArray[i]).closest(".talents");
 			$(talentsinfoArray[i]).width($(".talents").width()-70);
 		};
-	}resizeTalentsinfo();
+	}
 
 	$(".job-needed").click(function() {
 		$(".active").removeClass('active');
@@ -42,28 +54,60 @@ $(document).ready(function(){
 	})
 
 	// 弹出浮层选择
-	$(".item-details input").click(function(){
-		var url="load/duty.htm";
-		$("body").append("<div class='backdrop'></div><div class='layout'></div>")
-		$(".layout").load(url);
-		removelayer();
-	})
-
-	function removelayer(){
-		$(".backdrop").click(function() {
-			$(".layout,.backdrop").remove();
+		// 职位类型
+		$(".duty-htm").click(function(){
+			var url="load/duty.htm",name="职位类别";
+			appendlayer(name);
+			$(".layer-content").load(url);
+			removelayer();
 		});
-	}
+		// 学历要求
+		$(".education-background").click(function(){
+			var url="load/education-background.htm",name="学历要求";
+			appendlayer(name);
+			$(".layer-content").load(url);
+			removelayer();
+		});
+		// 薪资待遇
+		$(".money-duty").click(function(){
+			var url="load/money-duty.htm",name="薪资待遇";
+			appendlayer(name);
+			$(".layer-content").load(url);
+			removelayer();
+		});
+		// 语言能力
+		$(".language").click(function(){
+			var url="load/language.htm",name="语言能力";
+			appendlayer(name);
+			$(".layer-content").load(url);
+			removelayer();
+		});
 
+		
+		function appendlayer(name){
+			$("body").append(""+
+				"<div class='backdrop'></div>"+
+				"<div class='layer'>"+
+					"<div class='layer-title clearfix'>"+
+						"<div class='select-cancel clearfix'>"+
+							"<div class='cancel'><a type='button' href='javascript:;'>取消</a></div>"+
+							"<div class='action-title'>"+name+"</div>"+
+							"<div class='selecting'><a type='button' href='javascript:;'>确定</a></div>"+
+						"</div>"+
+					"</div>"+
+					"<div class='layer-content'></div>"+
+					"<div class='layer-footer'></div>"+
+				"</div>")		
+		}
+		function removelayer(){
+			$(document).delegate('.backdrop', 'click', function() {
+				$(".backdrop,.layer").remove();
+			});
+			$(document).delegate('.cancel a', 'click', function() {
+				$(".backdrop,.layer").remove();
+			});
+		}
 			
 
 
 });
-
-// 	myScroll = new IScroll('#wrapper', {
-// 					  snap:"li",
-//                       momentum:false,
-//                       hScrollbar:false,
-//                       vScrollbar:false});
-// }
-// document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
