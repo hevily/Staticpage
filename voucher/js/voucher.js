888b66fbd0e5030dae5ee62736fc8d68e3dbac5d
Lch-HMC-public <617054896@qq.com>
2015-10-16 18:55:33 +0800

    more loading animation

--- voucher/js/voucher.js
+++ voucher/js/voucher.js
@@ -1,23 +1,62 @@
 $(document).ready(function(){
-	$(".nav .select li").click(function() {
-		var _this = $(this);
-		if ($(".nav-opening")) {
-			$(".nav-opening").removeClass('nav-opening');
-			$(this).addClass('nav-opening');
-			$(".factor,.factor .select-factor").hide();
-			$(".factor").find(".select-factor").hide();
-			_this.closest(".select").next(".factor").show();
-			$(".nav .factor").find("li").find(".select-factor").eq(_this.index()).show();
+	$(".select li").click(function() {
+		if ($(this).hasClass("nav-open")) {
+			$(".backdrop,.layer").remove();
+			if($(this).hasClass("nav-loading")){
+				// $(this).addClass('nav-open');
+				$(".nav-loading").removeClass('nav-loading');
+				console.log("当前需关闭")
+			}else{
+				console.log("当前需加载");
+				$(this).addClass('nav-loading');
+				appendlayer();
+				$(".layer-content").load("load-page.htm");
+			}
 		}else{
-			$(this).addClass('nav-opening');
-			$(".factor,.factor .select-factor").hide();
-			_this.closest(".select").next(".factor").show();
-			$(".nav .factor").find("li").find(".select-factor").eq(_this.index()).show();
+			$(".backdrop,.layer").remove();
+			$(".nav-open").removeClass('nav-open nav-loading');
+			$(this).addClass('nav-open nav-loading');
+			appendlayer();
+			$(".layer-content").load("load-page.htm");
+
+			// $(".factor,.factor .select-factor").hide();
+			// _this.closest(".select").next(".factor").show();
+			// $(".nav .factor").find("li").find(".select-factor").eq(_this.index()).show();
 		};
-		console.log(_this.index());
-	});
-	$(".factor a").click(function() {
-		console.log("已点击排序条件")
-		$(".factor,.factor .select-factor").hide();
-	});
+		console.log($(this).index());
+		removelayer();
+		});
+	// $(".factor a").click(function() {
+	// 	console.log("已点击排序条件")
+	// 	$(".factor,.factor .select-factor").hide();
+	// });
+	function appendlayer(name,Classvalue){
+		$("body").append(""+
+			"<div class='backdrop'></div>"+
+			"<div class='layer'>"+
+				"<div class='layer-title clearfix'>"+
+					"<div class='select-cancel clearfix'>"+
+						"<div class='cancel'><a type='button' href='javascript:;'>取消</a></div>"+
+						"<div class='action-title'>"+name+"</div>"+
+						"<div class='selecting' Classvalue='"+Classvalue+"'><a type='button' href='javascript:;'>确定</a></div>"+
+					"</div>"+
+				"</div>"+
+				"<div class='layer-content'></div>"+
+				"<div class='layer-footer'></div>"+
+			"</div>");
+	}
+	function removelayer(){
+		// $('.backdrop').click(function() {
+		// 	$(".backdrop,.layer").remove();
+		// });
+		$(document).delegate('.backdrop', 'click', function() {
+			$(".backdrop,.layer").remove();
+			// $("."+removeClass).removeClass(''+removeClass+'');
+			$(".nav-loading").removeClass('nav-loading');
+		});
+		$(document).delegate('.cancel a', 'click', function() {
+			$(".backdrop,.layer").remove();
+		});
+
+	}
 })
