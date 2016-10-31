8d543a089410bb28557effc87c2a652c56d5daa3
TogetherWithM <617054896@qq.com>
2016-10-31 16:36:50 +0800

    影视界主页字体 字号 间距 更正

--- app/HMC-HomePage/js/head-nav.js
+++ app/HMC-HomePage/js/head-nav.js
@@ -2,10 +2,10 @@ window.onload = function(){
 	
 
 	$('#service').carousel({
-	  interval: 10000 
+	  	interval: 5000 
 	});
 	
-	$('#cooperation').constructor({
+	$('#cooperation').carousel({
 		interval:3000
 	});
 	
@@ -16,22 +16,43 @@ window.onload = function(){
 		var cN = 'stay-top';
 	//	滚动监听处理
 		window.onscroll = function(){
-			checkNav()
-			
+			var sTop = document.body.scrollTop
+			checkNav(sTop)
+			checkFilm(sTop)
 			
 		}
-		function checkNav(){
+		function checkNav(sTop){
 			var sH = slide.offsetHeight,
-				sTop = document.body.scrollTop
-				headH = header.offsetHeight;
-	//			console.log(sH+'|'+sTop+'|'+headH)
+				sTop = document.body.scrollTop,
+				headH = header.offsetHeight,
 				dCN = ' navbar navbar-default navbar-fixed-top ';
+//				console.log(sTop+'|'+sH+'|'+headH)
 			if(sTop > (sH - headH)){
-				(sTop > sH) ? false : header.setAttribute('class',dCN);
+				header.setAttribute('class',dCN);
 			}else{
 				header.setAttribute('class',dCN+cN);
 			}
 		}checkNav();
+		function checkFilm(sTop){
+			var goal = document.getElementById('film'),
+				gH = goal.offsetHeight;
+			var oT = goal.offsetTop;
+			var img1 = document.getElementById('img1'),
+				img2 = document.getElementById('img2'),
+				img3 = document.getElementById('img3')
+				
+//			console.log(oT+"--"+sTop);
+			if(sTop > (oT - gH)){
+				img1.setAttribute('class','film-img film-img-1 animated bounceInUp show');
+				img2.setAttribute('class','film-img film-img-2 animated bounceInDown show');
+				img3.setAttribute('class','film-img film-img-3 animated bounceInRight show')
+			}else{
+				img1.setAttribute('class','film-img film-img-1 animated hide');
+				img2.setAttribute('class','film-img film-img-2 animated hide');
+				img3.setAttribute('class','film-img film-img-3 animated hide');
+			}
+			
+		}
 	}());
 	
 	// slide touch gallery width base on jq
