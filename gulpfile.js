df67a386fb77e28a6c819b2930a9ea86ad559ca5
Lei <617054896@qq.com>
2016-12-25 18:34:22 +0800

    add vue project

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,40 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-
-// 静态服务器
-// gulp.task('browser-sync', function() {
-//     browserSync.init({
-//         server: {
-//             baseDir: "./"
-//         }
-//     });
-// });
-
-// 代理
-
-gulp.task('sync', function() {
-	// browserSync({
-	// 	files:"**",
-	// 	server:{
-	// 		proxy: "192.168.1.120/statichtml/moviehelper/app/"
-	// 	}
-	// });
-    browserSync.init({
-        proxy: "192.168.1.120/statichtml/moviehelper/app/"
-    });
-});
-gulp.task('default',['browserSync']);
+// conf
+	var conf ={
+		server:{
+			static:'',
+			proxy : 'work.com/moviehelper/app/vuejs/',
+			proxy_dir:'d:/WorkSpace/moviehelper/app/vuejs/'
+		}
+	}
+// require 
+	var gulp 	= require('gulp');
+	var bs 		= require('browser-sync').create();
+// tasks
+	// broser-sync init server
+	gulp.task('bsserver', function() {
+		conf.server.static !=''
+		?
+	    bs.init({
+	        server: "./app"
+	    })
+		:
+	    bs.init({
+	        proxy: conf.server.proxy
+	    });
+	});
+	// gulp.wath
+	gulp.task('watch',function () {
+		gulp.watch(conf.server.proxy_dir+'**/*.html',function (event) {
+			console.log('File ' + event.path + ' was ' + event.type + ', running tasks...')
+			return gulp.src(event.path)
+				.pipe(bs.reload({stream:true}))
+		});
+		gulp.watch(conf.server.proxy_dir+'**/*.css',function (event) {
+			console.log('File ' + event.path + ' was ' + event.type + ', running tasks...')
+			return gulp.src(event.path)
+				.pipe(bs.reload({stream:true}))
+			// bs.reload() too low and skip away by node pipe stream
+		})
+	})
+	// default task
+	gulp.task('default', ['bsserver','watch']);
