2d7f661543420871f5ffebb3c438775f82ec452c
HMC_mac <617054896@qq.com>
2017-10-25 16:01:25 +0800

    storganiser done rewrite logitics

--- app/Public/base.js
+++ app/Public/base.js
@@ -6,6 +6,21 @@ function pageBack(argument) {
 		console.log('addnew');
 		pageBack();
 	})
-
+	//设备检测
+function detectmob() {
+    if( navigator.userAgent.match(/Android/i)
+    || navigator.userAgent.match(/webOS/i)
+    || navigator.userAgent.match(/iPhone/i)
+    || navigator.userAgent.match(/iPad/i)
+    || navigator.userAgent.match(/iPod/i)
+    || navigator.userAgent.match(/BlackBerry/i)
+    || navigator.userAgent.match(/Windows Phone/i)
+    ){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
 // 院友
